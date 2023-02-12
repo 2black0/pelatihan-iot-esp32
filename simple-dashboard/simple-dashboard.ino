96b4ce0cd834eb471a7658231043a8776ebe6349
2black0 <2black0@gmail.com>
2023-02-12 17:15:02 +0700

    update

---
+++ simple-dashboard/simple-dashboard.ino
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <ESPDash.h>
+
+const char* ssid = "Wifi-Roboto";
+const char* password = "arDY1234";
+
+AsyncWebServer server(80);
+ESPDash dashboard(&server); 
+
+Card temperature(&dashboard, TEMPERATURE_CARD, "Temperature", "°C");
+Card humidity(&dashboard, HUMIDITY_CARD, "Humidity", "%");
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+      Serial.printf("WiFi Failed!\n");
+      return;
+  }
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+}
+
+void loop() {
+  temperature.update((int)random(0, 50));
+  humidity.update((int)random(0, 100));
+  dashboard.sendUpdates();
+  delay(3000);
+}

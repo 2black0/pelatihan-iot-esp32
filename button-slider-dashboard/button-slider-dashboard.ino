9a210c9e52c53b30ca312f51fa02943090b97929
2black0 <2black0@gmail.com>
2023-02-12 17:31:53 +0700

    update

--- button-slider-dashboard/button-slider-dashboard.ino
+++ button-slider-dashboard/button-slider-dashboard.ino
@@ -10,11 +10,16 @@ const char* password = "Pass_Wifi";
 AsyncWebServer server(80);
 ESPDash dashboard(&server); 
 
+Card button(&dashboard, BUTTON_CARD, "Test Button");
+Card slider(&dashboard, SLIDER_CARD, "Test Slider", "", 0, 255);
 Card temperature(&dashboard, TEMPERATURE_CARD, "Temperature", "°C");
 Card humidity(&dashboard, HUMIDITY_CARD, "Humidity", "%");
 
+uint8_t led = BUILTIN_LED;
+
 void setup() {
   Serial.begin(115200);
+
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   if (WiFi.waitForConnectResult() != WL_CONNECTED) {
@@ -23,6 +28,23 @@ void setup() {
   }
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
+
+  pinMode(led, OUTPUT);
+	digitalWrite(led, LOW);
+
+  button.attachCallback([&](bool value){
+    Serial.println("Button Triggered: "+String((value)?"true":"false"));
+    button.update(value);
+    digitalWrite(led, value);
+    dashboard.sendUpdates();
+  });
+
+  slider.attachCallback([&](int value){
+    Serial.println("Slider Triggered: "+String(value));
+    slider.update(value);
+    dashboard.sendUpdates();
+  });
+
   server.begin();
 }
 
